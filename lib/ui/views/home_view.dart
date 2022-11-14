ff35ee988c9173849a8e064c1d4d3d1ce66034de
Joni_Juliansah <87921492+JohnTrader@users.noreply.github.com>
2022-11-14 20:44:41 +0700

    Update home_view.dart

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:shame_app/ui/views/home_page.dart';
 import 'package:dart_amqp/dart_amqp.dart';
 import 'package:flutter/material.dart';
 import 'package:shame_app/services/rmq_service.dart';
 import 'package:shame_app/ui/model/value_model.dart';
 import 'package:shame_app/ui/widget/card_view.dart';
+import 'package:intl/intl.dart';
+import 'package:shame_app/sensor_model.dart';
 
 class HomeView extends StatefulWidget {
   String user;
@@ -17,62 +20,182 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  RMQService rmqService = RMQService();
+  List<SensorModel> _list = [];
+  List<String> _sensor = ['Humidity', 'Temperature'];
+  final _random = new Random();
+  bool rmq_status = true;
   String payload = "";
-  late Client client;
   String soil_serial = "";
   String soil_value = "";
   String soil_status = "";
-  bool check_status = false;
-  //String pompa_serial = "";
-  //String pompa_value = "";
 
-  List<ValueModel> device = [];
-  void connect() {
-    try {
-      ConnectionSettings settings = ConnectionSettings(
-        host: 'rmq2.pptik.id',
-        authProvider: PlainAuthenticator(widget.user, widget.pass),
-        virtualHost: widget.vhost,
+  String pompa_serial = "";
+  String pompa_value = "";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    consumeSoil();
+  }
+  void consumeSoil(){
+    try{
+      ConnectionSettings settings = new ConnectionSettings(
+          host: "rmq2.pptik.id",
+          authProvider: new PlainAuthenticator(widget.user, widget.pass),
+          virtualHost: widget.vhost
       );
-      client = Client(settings: settings);
 
-      client.connect().then((value) {
+      Client client = new Client(settings: settings);
+
+      client.errorListener((error) {print("dsa${error.toString()}"); });
+      client.connect().catchError((Object error){
+        print("dsa ${error.toString()}");
         setState(() {
-          check_status = true;
-          //MyHomePage();
-          //data();
-          //data_pompa();
+          rmq_status = false;
         });
       });
-    } catch (e) {
-      print("kesalahan 344or $e");
-    }
-  }
-/*
-  void data() {
-    client
-        .channel()
-        .then((Channel channel) => channel.queue("Sensor_PZEM004T", durable: true))
-        .then((Queue queue) => queue.consume())
-        .then((Consumer consumer) => consumer.listen((AmqpMessage message) {
-      print("test ${message.payloadAsString}");
-      //setValueSoil(message.payloadAsString);
-      setState(() {
-        payload = message.payloadAsString;
+      client.connect().then((value){
+        setState(() {
+          rmq_status = true;
+        });
       });
-    }));
-  } */
-/*
-  void data_pompa() {
-    client
-        .channel()
-        .then((Channel channel) => channel.queue("Aktuator", durable: true))
-        .then((Queue queue) => queue.consume())
-        .then((Consumer consumer) => consumer.listen((AmqpMessage message) {
-      print("test ${message.payloadAsString}");
-      setValuePompa(message.payloadAsString);
-    }));
+
+      client
+          .channel()
+          .then((Channel channel) => channel.queue("Sensor", durable: true))
+          .then((Queue queue) => queue.consume())
+          .then((Consumer consumer) => consumer.listen((AmqpMessage message) {
+        print("test ${message.payloadAsString}");
+        setValuePompa(message.payloadAsString);
+        setState(() {
+          payload = message.payloadAsString;
+        });
+      }));
+      client
+          .channel()
+          .then((Channel channel) => channel.queue("Log", durable: true))
+          .then((Queue queue) => queue.consume())
+          .then((Consumer consumer) => consumer.listen((AmqpMessage message) {
+        print("test ${message.payloadAsString}");
+        setValueSoil(message.payloadAsString);
+        setState(() {
+          payload = message.payloadAsString;
+        });
+      }));
+    }on Exception catch(e){
+      print("[x]Received False ${e.toString()}");
+    }
+
+  }
+  Widget userList(BuildContext context, int index) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blue),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        color: Colors.transparent,
+      ),
+      width: double.infinity,
+      height: 120,
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  _list[index].title,
+                  style: TextStyle(
+                      height: 1.5,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(_list[index].value,
+                    style: TextStyle(
+                        height: 1.5,
+                        color: Colors.black54,
+                        fontSize: 18,
+                        letterSpacing: .3)),
+                SizedBox(
+                  height: 6,
+                ),
+                Text('${_list[index].timestamp}',
+                    style: TextStyle(
+                        height: 2.5,
+                        color: Colors.black45,
+                        fontSize: 13,
+                        letterSpacing: .3)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SHA.ME"),
+      ),
+      body: rmq_status ? Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: _list.length,
+              itemBuilder: (BuildContext context, int index) {
+                return userList(context, index);
+              },
+            ),
+          )
+        ],
+      ):
+      Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text("RMQ Not Conneceted"),
+            Text("Please Check Credential")
+          ],
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+
+  void randomSensor(String title,String serial,String value) {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('EEE,d MMM yyyy, HH:mm:ss a').format(now);
+    var sensor = _sensor[_random.nextInt(_sensor.length)];
+    print(sensor);
+    setState(() {
+      if (sensor == 'Humidity') {
+        _list.insert(
+            0, SensorModel(title,serial, randomValue(value), formattedDate));
+      } else {
+        _list.insert(
+            0, SensorModel(title,serial, randomValue(value), formattedDate));
+      }
+    });
+  }
+
+  String randomValue(String sensor) {
+
+    if (sensor == 'Humidity') {
+      return '${sensor} g/m\u00B3';
+    } else {
+
+      return 'Status : $sensor';
+    }
   }
 
   void setValuePompa(String message) {
@@ -85,8 +208,7 @@ class _HomeViewState extends State<HomeView> {
       } else if (cek_value == 0) {
         pompa_value = 'OFF';
       }
-      device.add(
-          ValueModel(pompa_serial, "POMPA", cek_value.toString(), pompa_value));
+      randomSensor("Pompa", pompa_serial, pompa_value);
     });
   }
 
@@ -103,42 +225,8 @@ class _HomeViewState extends State<HomeView> {
       } else if (cek >= 350 && cek <= 700) {
         soil_status = 'Normal';
       }
-      device.add(ValueModel(soil_serial, "SOIL", soil_value, soil_status));
+      randomSensor("Soil", soil_serial,soil_value);
     });
   }
-*/
-  @override
-  void initState() {
-    // TODO: implement initState
-    // data();
-    // data_pompa();
-    connect();
-    //MyHomePage();
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-            child: Padding(
-                padding: EdgeInsets.all(10),
-                child: check_status?ListView.builder(
-                  reverse: true,
-                  itemCount: device.length,
-                  itemBuilder: (context,idx){
-
-                    return ContentCardView(device: device[idx],);
-                  },
-                ):Container(
-                  child: Center(
-                    child: Text("RMQ Not Connected \nPlease check your credential"),
-                  ),
-                )
-            )
-        )
-    );
-
-    // TODO: implement build
-    throw UnimplementedError();
-  }
 }
