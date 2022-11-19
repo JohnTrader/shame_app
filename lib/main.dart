7ef3866c1fe5ba5923daf85e43ffcf5e42a24b85
Joni_Juliansah <87921492+JohnTrader@users.noreply.github.com>
2022-11-20 01:26:10 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shame_app/themes/colors.dart';
 import 'package:shame_app/splashscreen.dart';
+import 'package:shame_app/ui/views/home_dashboard.dart';
 //import 'package:shame_app/ui/views/home_page.dart';
 //import 'package:shame_app/ui/views/home_view.dart';
 //import 'package:shame_app/ui/views/login_view.dart';
@@ -22,7 +23,8 @@ class MyApp extends StatelessWidget {
       //home: HomeView(user: 'TMDG2022',pass: 'TMDG2022',vhost: '/TMDG2022'),
       //home: const loginScreen(),
       //home: MyHomePage(),
-      home: SplashScreen(),
+      //home : const HomePage(),
+      home: const SplashScreen(),
     );
   }
 }
