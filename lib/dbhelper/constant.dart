a5fa9a4bcb45f2e880e33048492203856010ab4b
Joni_Juliansah <87921492+JohnTrader@users.noreply.github.com>
2022-11-24 08:05:37 +0700

    Create constant.dart

---
+++ lib/dbhelper/constant.dart
@@ -0,0 +1,7 @@
+//user = "shame_user"
+//password = "W3HyQdZHGk7ePeUs"
+
+const MONGO_CONN_URL =
+    "mongodb+srv://shame_user:W3HyQdZHGk7ePeUs@cluster0.hsr1eum.mongodb.net/?retryWrites=true&w=majority";
+const USER_COLLECTION = "Data_Sensor";
+
