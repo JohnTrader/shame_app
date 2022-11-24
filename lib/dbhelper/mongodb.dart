5702a83688964a8aee77093e64c63df926fe23f3
Joni_Juliansah <87921492+JohnTrader@users.noreply.github.com>
2022-11-24 14:33:02 +0700

    Update mongodb.dart

--- lib/dbhelper/mongodb.dart
+++ lib/dbhelper/mongodb.dart
@@ -3,11 +3,31 @@ import 'package:mongo_dart/mongo_dart.dart';
 import 'package:shame_app/dbhelper/constant.dart';
 
 class MongoDatabase {
-  static var db, userCollection;
+
   static connect() async{
-    db = await Db.create(MONGO_CONN_URL);
+    var db = await Db.create(MONGO_CONN_URL);
     await db.open();
     inspect(db);
-    userCollection = db.collection(USER_COLLECTION);
+    var status = db.serverStatus();
+    print(status);
+    var collection = db.collection(USER_COLLECTION);
+    // await collection.insertOne({
+    //   "Sensor" : "PZEM004T",
+    //   "Energy" : "0.685",
+    //   "Time" : "24-Nov-2022 14:40"
+    // },
+    //     {
+    //     "username" : "np2",
+    //     "name" : "Max Payne2",
+    //     "email" : "maxpayne2@gmail.com"
+    //     }
+    //     );
+    // print(await collection.find().toList());
+    //
+    // await collection.update(where.eq('username','np'), modify.set('name','Max Pax'));
+    print(await collection.find().toList());
+
+    //await collection.deleteOne({"username": "np"});
+    //await collection.deleteMany({"username": "np2"});
   }
 }
