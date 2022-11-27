7f5c6aa2b0dca65b0573992e94ec5cd8780f1aee
Joni_Juliansah <87921492+JohnTrader@users.noreply.github.com>
2022-11-27 22:50:50 +0700

    Update mongodb.dart

--- lib/dbhelper/mongodb.dart
+++ lib/dbhelper/mongodb.dart
@@ -1,38 +1,82 @@
 import 'dart:developer';
 import 'package:intl/intl.dart';
 import 'package:mongo_dart/mongo_dart.dart';
+import 'package:shame_app/MongoDBModel.dart';
 import 'package:shame_app/dbhelper/constant.dart';
 
+/*
 class MongoDatabase {
 
-  static connect(String payload) async{
+  //static connect(String payload) async {
+  static connect(String payload) async {
     var db = await Db.create(MONGO_CONN_URL);
     await db.open();
     inspect(db);
     //var status = db.serverStatus();
     //print(status);
     var collection = db.collection(USER_COLLECTION);
-    //var payload;
     DateTime now = DateTime.now();
     String formattedDate = DateFormat('d-MMM-yyyy HH:mm:ss').format(now);
     await collection.insertOne({
+        "Sensor" : "Energy",
+        "KWH" : payload,
+        "Time" : formattedDate
+         },
+     );
+
+      //print(await collection.find().toList());
+      print("[x] Sukses Kirim Data ke MongoDB ");
+      // await collection.update(where.eq('username','np'), modify.set('name','Max Pax'));
+      //print(await collection.find().toList());
+      //await collection.deleteOne({"username": "np"});
+      //await collection.deleteMany({"Sensor": "Energy"});
+    }
+  }
+*/
+
+
+
+class MongoDatabase{
+   static var db, userCollection;
+
+   static connect() async{
+     db = await Db.create(MONGO_CONN_URL);
+     await db.open();
+     userCollection = db.collection(USER_COLLECTION);
+
+   }
+
+   static insertData(String payload) async{
+
+     DateTime now = DateTime.now();
+     String formattedDate = DateFormat('d-MMM-yyyy HH:mm:ss').format(now);
+     await userCollection.insertOne({
        "Sensor" : "Energy",
        "KWH" : payload,
        "Time" : formattedDate
-     },
-    //     {
-    //     "username" : "np2",
-    //     "name" : "Max Payne2",
-    //     "email" : "maxpayne2@gmail.com"
-    //     }
-         );
-     //print(await collection.find().toList());
+       },
+     );
      print("[x] Sukses Kirim Data ke MongoDB ");
-    // await collection.update(where.eq('username','np'), modify.set('name','Max Pax'));
-    //print(await collection.find().toList());
+   }
 
-    //await collection.deleteOne({"username": "np"});
-    //await collection.deleteMany({"username": "np2"});
-  }
+   static Future<List<Map<String, dynamic>>> getData() async{
+     final arrData = await userCollection.find().toList();
+     return arrData;
+   }
+
+   static Future <String> insert(MongoDbModel data) async{
+     try {
+       var result = await userCollection.insertOne(data.toJson());
+       //print(await userCollection.find().toList());
+       if(result.isSuccess) {
+         return "Data Inserted";
+
+       } else {
+         return "Something Wrong While Inserting Data...";
+       }
+     } catch (e) {
+       print(e.toString());
+       return e.toString();
+     }
+   }
 }
-
