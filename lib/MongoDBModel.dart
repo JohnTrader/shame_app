db9d73d154554a36079d4c1668b4bf82eb327362
Joni_Juliansah <87921492+JohnTrader@users.noreply.github.com>
2022-11-28 00:24:14 +0700

    Update MongoDBModel.dart

--- lib/MongoDBModel.dart
+++ lib/MongoDBModel.dart
@@ -9,29 +9,42 @@ String mongoDbModelToJason(MongoDbModel data) => json.encode(data.toJson());
 
 class MongoDbModel {
   MongoDbModel ({
-    required this.id,
-    required this.firstName,
-    required this.lastName,
-    required this.address,
+     required this.id,
+     required this.firstName,
+     required this.lastName,
+     required this.address,
+    //required this.id,
+    //required this.Sensor,
+    //required this.KWH,
+    //required this.Time,
   });
 
-  ObjectId id;
-  //String id;
-  String firstName;
-  String lastName;
-  String address;
+   ObjectId id;
+   String firstName;
+   String lastName;
+   String address;
+    //ObjectId id;
+    //String Sensor;
+    //String KWH;
+    //String Time;
 
   factory MongoDbModel.fromJson(Map<String, dynamic> json) => MongoDbModel(
     id: json["_id"],
     firstName: json["firstName"],
     lastName: json["lastName"],
     address: json["address"],
+     //firstName: json["Sensor"],
+     //lastName: json["KWH"],
+     //address: json["Time"],
   );
 
   Map<String,dynamic> toJson() => {
-      "_id": id,
-      "firstName": firstName,
-      "lastName": lastName,
-      "address": address,
+    "_id": id,
+    "firstName": firstName,
+    "lastName": lastName,
+    "address": address,
+    //"Sensor": firstName,
+    //"KWH": lastName,
+    //"Time": address,
   };
 }
