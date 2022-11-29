ce77c3d88666b1a612e4953f5c5d80c9f2dc7367
Joni_Juliansah <87921492+JohnTrader@users.noreply.github.com>
2022-11-29 09:54:09 +0700

    Create living_room_category.dart

---
+++ lib/smarthome_ui/src/model/living_room_category.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LivingRoomCategory{
+  IconData iconData;
+  String title;
+
+  LivingRoomCategory(this.iconData, this.title);
+}
+
+List<LivingRoomCategory> categoryItems = [
+  LivingRoomCategory(Icons.ac_unit, "AC"),
+  LivingRoomCategory(Icons.tv, "TV"),
+  LivingRoomCategory(Icons.lightbulb, "Light"),
+  LivingRoomCategory(Icons.speaker, "Home Theater"),
+  LivingRoomCategory(Icons.music_note, "Music"),
+  LivingRoomCategory(Icons.videocam, "CCTV"),
+];
